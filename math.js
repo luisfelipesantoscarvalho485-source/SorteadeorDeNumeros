af11d5f885f8bb3a19dab1b67380b253cc9a043d
Miguelzinho10 <luisfelipesantoscarvalho485@gmail.com>
2026-06-21 14:39:38 -0300

    Update math.js

--- math.js
+++ math.js
@@ -7,11 +7,12 @@ function sortear() {
     if(valor1 <= valor2){
     alert("O Primeiro valor nao pode ser maior que o segundo")
     }
+    else{
 
     const valor1 = Math.ceil(document.querySelector(".primeiro").value)
     const valor2 = Math.floor(document.querySelector(".max").value)
 
     const result = Math.floor(Math.random() * (valor2 - valor1 + 1)) +valor1;
     
-    alert(result)
+    alert(result)}
 }
